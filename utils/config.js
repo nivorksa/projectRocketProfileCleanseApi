cadba15798456dd5871e23307b61d50de8ff8c04
nivorksa <nivorksa@outlook.com>
2025-07-31 05:10:36 +0530

    fix: Correct API and CORS URLs in config for proper deployment

--- utils/config.js
+++ utils/config.js
@@ -1,19 +1,19 @@
 // Define API Base URL for axios requests (local or live)
 const apiBaseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://projectrocketprofilecheckapi.onrender.com/api/"
+    ? "https://projectrocketprofilecleanseapi.onrender.com/api/"
     : "http://localhost:8800/api/";
 
 // Define Socket URL for socket.io connections (local or live)
 // const socketUrl =
 //   process.env.NODE_ENV === "production"
-//     ? "https://projectrocketprofilecheckapi.onrender.com"
+//     ? "https://projectrocketprofilecleanseapi.onrender.com"
 //     : "http://localhost:8800";
 
 // Define allowed origins for CORS (frontend URLs)
 const allowedOrigins = [
   "http://localhost:3000", // Local frontend for development
-  "https://projectrocketprofilecheckdev.netlify.app", // Deployed frontend on Netlify
+  "https://projectrocketprofilecleansedev.netlify.app", // Deployed frontend on Netlify
 ];
 
 export {
